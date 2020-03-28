b57747fe415a4b06e61fa2e3e57986dc31cf2e9c
George Stambulyants <george.stamb28@gmail.com>
2020-03-28 11:52:01 +0300

    Created model URL adn ran migrations

--- url_shortener_project/core/models.py
+++ url_shortener_project/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class URL(models.Model):
+    original_url = models.URLField(max_length=200, unique=True)
+    shortened_url = models.URLField(max_length=200, unique=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.original_url} => {self.shortened_url}'
