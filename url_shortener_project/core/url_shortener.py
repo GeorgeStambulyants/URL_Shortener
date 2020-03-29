9da204f3ec82b42b211746985b30ca5a1d3ed6a6
George Stambulyants <george.stamb28@gmail.com>
2020-03-29 12:45:33 +0300

    Improved url_shortener function. now it cannot generate shortened url that already exists

--- url_shortener_project/core/url_shortener.py
+++ url_shortener_project/core/url_shortener.py
@@ -4,11 +4,23 @@ import string
 from django.conf import (
     settings,
 )
+from .models import URL
 
 
 def url_shortener(original_url):
     shortened_url = settings.ALLOWED_HOSTS[0] + ':8000/'
     for _ in range(6):
-        shortened_url += random.choice(string.ascii_letters)
-    
-    return shortened_url
+        shortened_url += random.choice(
+            string.ascii_letters + string.digits
+        )
+    while True:
+        try:
+            URL.objects.get(shortened_url=shortened_url)
+            # if such URL already exists, make another one
+            shortened_url = ''.join(
+                random.shuffle(
+                    list(shortened_url)
+                )
+            )
+        except URL.DoesNotExist:
+            return shortened_url
