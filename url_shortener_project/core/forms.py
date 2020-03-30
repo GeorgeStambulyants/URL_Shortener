9b8e167df310069222e788553392fd94ff7cfc40
George Stambulyants <george.stamb28@gmail.com>
2020-03-30 16:16:21 +0300

    Added clean method to the form. Now there is now errors with uniqueness of slug

--- url_shortener_project/core/forms.py
+++ url_shortener_project/core/forms.py
@@ -1,6 +1,27 @@
 from django import forms
+from .models import FriendlyURL, URL
 
 
 class URLForm(forms.Form):
     url_to_shorten = forms.URLField(label='URL to shorten')
     word_for_slug = forms.SlugField(required=False)
+
+    def check_unique_slug(self, slug, object_list):
+        for obj in object_list:
+            if obj.friendly_shortened_url.endswith(slug):
+                raise forms.ValidationError('Such slug already exists')
+
+    def clean_word_for_slug(self):
+        cd = self.cleaned_data
+        word_for_slug = cd['word_for_slug']
+        original_url = cd['url_to_shorten']
+
+        if word_for_slug:    
+            try:
+                url_obj = URL.objects.get(original_url=original_url)
+                friendly_url_objs = FriendlyURL.objects.exclude(original_url=url_obj.pk)
+                self.check_unique_slug(word_for_slug, friendly_url_objs) 
+            except URL.DoesNotExist:
+                friendly_url_objs = FriendlyURL.objects.all()
+                self.check_unique_slug(word_for_slug, friendly_url_objs)
+        return word_for_slug
